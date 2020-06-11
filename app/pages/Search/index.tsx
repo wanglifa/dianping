29e4885063627804a8eb10484e15b8eb867ed93b
wanglifa <wanglifa1995@gmail.com>
2020-06-11 17:51:16 +0800

    搜索组件

---
+++ app/pages/Search/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+const Search: React.FC = () => {
+  return (
+    <div>
+      notFound
+    </div>
+  )
+}
+export default Search;
--- app/router/routerMap.tsx
+++ app/router/routerMap.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import List from '../pages/List';
 import Detail from '../pages/Detail';
 import City from '../pages/City'
+import Search from '../pages/Search';
 
 const RouterMap: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const RouterMap: React.FC = () => {
       <Route path={"/"} component={Home} exact/>
       <Route path={"/city"} component={City} exact/>
       <Route path={"/list"} component={List} exact/>
+      <Route path={"/search/:category/:keyword?"} component={Search} exact/>
       <Route path={"/detail/:id"} component={Detail} exact/>
     </Router>
   )
